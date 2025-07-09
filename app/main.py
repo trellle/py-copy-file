eebb1bca28cd5e6f0c81ae8e32fa7d17408a110d
yarik <super.racing@ukr.net>
2025-07-10 00:32:47 +0300

    changed validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     params = command.split()
-    if params[1] == params[2] or params[2] == "" or params[1] == "":
+    if len(params) < 3 or params[1] == params[2]:
         return
     file1 = open(params[1], "r")
     content = file1.read()
