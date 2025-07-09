0aaf78f27d8d6706bf52270d67874ff22044a4d7
yarik <super.racing@ukr.net>
2025-07-10 00:26:33 +0300

    created copy function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    params = command.split()
+    if params[1] == params[2]:
+        return
+    file1 = open(params[1], "r")
+    content = file1.read()
+    file1.close()
+    file2 = open(params[2], "w")
+    file2.write(content)
