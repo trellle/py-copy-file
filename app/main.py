d0d748fe904a77692a865ca44aad1ac7c2997af1
yarik <super.racing@ukr.net>
2025-07-10 08:25:20 +0300

    changed if statement

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 def copy_file(command: str) -> None:
     params = command.split()
-    if len(params) < 3 or params[1] == params[2]:
+    if len(params) != 3 or params[1] == params[2]:
         return
     try:
         file1 = open(params[1], "r")
